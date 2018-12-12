c049f8270ccb45c913d2c929fde0d33642d88d54
Nicholas <nicholas.s.griffen@gmail.com>
2018-12-12 10:03:57 -0700

    define posts model

---
+++ brewniverse/posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+# Auto-incrementing ID included implicitly 
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    picture = models.CharField(max_length=255)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
