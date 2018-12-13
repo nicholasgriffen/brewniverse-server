8ad77bd3813e41b72def5a4afc3d34ec59ac9958
Nicholas <nicholas.s.griffen@gmail.com>
2018-12-13 08:11:18 -0700

    nested tags

--- posts/models.py
+++ posts/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 # Auto-incrementing ID included implicitly 
 
+class Tag(models.Model):
+    tag = models.CharField(max_length=32)
+    posts = models.ManyToManyField('posts.Post')
+    posts.null = True
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
     content = models.TextField()
-    picture = models.CharField(max_length=255)
-    score = models.IntegerField()
-    rating = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    picture = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    score = models.IntegerField()
+    tags = models.ManyToManyField('posts.Tag')    
     updated_at = models.DateTimeField(auto_now=True)
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,15 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from posts.models import Post 
+from posts.models import Post, Tag 
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Tag 
+        fields = ['tag']
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.id')
+    tags = TagSerializer(many=True, read_only=True)
     class Meta: 
         model = Post
-        fields = ('id', 'title', 'content', 'picture', 'score', 'rating', 'author')
+        fields = ('id', 
+                'author',
+                'content', 
+                'picture', 
+                'rating', 
+                'score',
+                'tags', 
+                'title')
 
-    author = serializers.ReadOnlyField(source='author.id')
 
 class UserSerializer(serializers.ModelSerializer):
+    posts = PostSerializer(many=True, read_only=True)
     class Meta: 
         model = User 
-        fields = ('id', 'username', 'email', 'posts')
+        fields = ('id', 
+                'username', 
+                'email', 
+                'posts')
