27e41b3b5cad3610911ca77f6c8894c4de4f3d5a
Nick Griffen <nicholas.s.griffen@gmail.com>
2018-12-14 12:02:13 -0700

    Update permissions.py

--- posts/permissions.py
+++ posts/permissions.py
@@ -16,4 +16,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if hasattr(obj, 'author'):
             return obj.author == request.user
         else:
-            return obj == request.user
+            return True
