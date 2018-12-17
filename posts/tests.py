1b8f17f48719a7b4bebc44e140075673dbabfe02
Nicholas <nicholas.s.griffen@gmail.com>
2018-12-17 11:08:57 -0700

    test GET /users/:id

--- posts/tests.py
+++ posts/tests.py
@@ -1,12 +1,12 @@
-from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+
 from posts.models import Brewser
 
 class BrewserTests(APITestCase):
     def test_create_brewser(self):
         """
-        Ensure we can create a new Brewser object.
+        POST to /users/ creates a new Brewser.
         """
         url = '/users/'
         data = {
@@ -19,3 +19,24 @@ class BrewserTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Brewser.objects.count(), 1)
         self.assertEqual(Brewser.objects.get().username, 'Digijan')
+    
+    def test_get_brewser(self):
+        """
+        GET to /users/:id retrieves Brewser matching :id
+        """ 
+        testUser = Brewser.objects.create(
+            username='Digijan',
+            email='digijan@test.net',
+            picture='http://cdn.forum280.org/logos/forum280_logo_no_tagline.png',
+            password='janpass2018'
+        )
+        url = '/users/1'
+        response = self.client.get(url)
+        self.assertEqual(response.data, {
+            'id': 1,
+            'username': 'Digijan',
+            'email': 'digijan@test.net',
+            'picture': 'http://cdn.forum280.org/logos/forum280_logo_no_tagline.png',
+            'posts': [],
+            'channels': []
+        })
