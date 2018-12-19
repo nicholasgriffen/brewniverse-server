7f04e614a60ae57252efc42ef1025d1c53678920
Nicholas <nicholas.s.griffen@gmail.com>
2018-12-18 21:27:02 -0700

    fill and submit signup form

--- posts/tests/test_client.py
+++ posts/tests/test_client.py
@@ -4,7 +4,16 @@ from rest_framework.test import APITestCase
 
 class ClientTest(APITestCase):
     baseClientUrl = 'http://parallel-brewniverses.surge.sh'
-    
+    signupUrl = '/#/signup'
+    data = [
+        user: {
+            email: "digijan@test.net",
+            name: "digijan",
+            password: "development",
+            confPass: "development"
+        }
+    ]
+
     def setUp(self):
         options = webdriver.firefox.options.Options()
         options.set_headless()
@@ -13,4 +22,25 @@ class ClientTest(APITestCase):
 
     def testPageTitle(self):
         self.browser.get(self.baseClientUrl)
-        self.assertIn('Parallel', self.browser.title)
+        self.assertEqual('Parallel Brewniverses', self.browser.title)
+    
+    def testSignupForm(self):
+        self.brow
+        form = self.browser.find_element_by_tag_name('form')
+        form.submit()ser.get(self.baseClientUrl + self.signupUrl)
+        user = self.data['user']
+
+        email = self.browser.find_element_by_name('email')
+        email.send_keys(user.email)
+
+        name = self.browser.find_element_by_name('name')
+        name.send_keys(user.name)
+        
+        password = self.browser.find_element_by_name('pass')
+        password.send_keys(user.password)
+        
+        confPass = self.browser.find_element_by_name('confPass')
+        confPass.send_keys(user.confPass)
+
+        form = self.browser.find_element_by_tag_name('form')
+        form.submit()
