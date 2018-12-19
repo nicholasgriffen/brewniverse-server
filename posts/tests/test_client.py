e30ad4c463d19ebff359d739b0fc3b227dc62b2f
Nicholas <nicholas.s.griffen@gmail.com>
2018-12-18 23:13:16 -0700

    sign up a user

--- posts/tests/test_client.py
+++ posts/tests/test_client.py
@@ -1,46 +1,61 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from rest_framework.test import APITestCase
 
 
 class ClientTest(APITestCase):
-    baseClientUrl = 'http://parallel-brewniverses.surge.sh'
-    signupUrl = '/#/signup'
-    data = [
-        user: {
-            email: "digijan@test.net",
-            name: "digijan",
-            password: "development",
-            confPass: "development"
+
+    data = {
+        'urls': {
+            'base': 'http://parallel-brewniverses.surge.sh/#/',
+            'signup': 'signup',
+            'home': 'all',
+        },
+        
+        'user': {
+            'email': "dijan@test.net",
+            'name': "dija",
+            'password': "development",
         }
-    ]
+    }
 
     def setUp(self):
         options = webdriver.firefox.options.Options()
-        options.set_headless()
+        # options.set_headless()
         self.browser = webdriver.Firefox(options=options)
         self.addCleanup(self.browser.quit)
 
     def testPageTitle(self):
-        self.browser.get(self.baseClientUrl)
+        self.browser.get(self.data['urls']['base'])
         self.assertEqual('Parallel Brewniverses', self.browser.title)
     
     def testSignupForm(self):
-        self.brow
-        form = self.browser.find_element_by_tag_name('form')
-        form.submit()ser.get(self.baseClientUrl + self.signupUrl)
+        # sign up
+        urls = self.data['urls']
         user = self.data['user']
-
+        
+        self.browser.get(urls['base'] + urls['signup'])
+        
         email = self.browser.find_element_by_name('email')
-        email.send_keys(user.email)
+        email.send_keys(user['email'])
 
         name = self.browser.find_element_by_name('name')
-        name.send_keys(user.name)
+        name.send_keys(user['name'])
         
         password = self.browser.find_element_by_name('pass')
-        password.send_keys(user.password)
+        password.send_keys(user['password'])
         
         confPass = self.browser.find_element_by_name('confPass')
-        confPass.send_keys(user.confPass)
+        confPass.send_keys(user['password'])
+        
+        confPass.submit()
+        
+        try:
+            element = WebDriverWait(self.browser, 10).until(
+                EC.url_to_be(urls['base'] + urls['home'])
+            )
 
-        form = self.browser.find_element_by_tag_name('form')
-        form.submit()
+        finally:
+            self.browser.quit()
